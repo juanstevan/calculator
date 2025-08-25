5d2bc5193bf1b9b33c4bd2028709e3490948b7d3
Juan Stevan <juanstevan@live.com>
2025-08-25 17:36:28 -0400

    Feat: 1. Non selectable buttons 2. Add 0 when dot is the first char 3. Format the numbers adding comma

--- script.js
+++ script.js
@@ -11,19 +11,44 @@ Object.defineProperty(window, 'content', {
 numbers.forEach((number) => 
     number.addEventListener('click', function() {
         if (this.id == 'dot' && content.includes('.')) return;
-        content += this.textContent
+        if (this.id == 'dot' && content == '') return content += '0.';
+        
+        content += this.textContent;
+
+        adjustText();
     })
 );
 
 document.addEventListener('keydown', (event) => {
     let key = event.key;
     if (key == '.' && content.includes('.') || key !== '.' && isNaN(+key)) return;
+    if (content == '' && key == '.') return content += '0.';
 
     key == ' ' ?
         content = '' : 
-        content += key
+        content += key ;
+
+    adjustText();
 });
 
 clear.addEventListener('click', (event) => {
     content = "";
 });
+
+function adjustText() {
+    let textList = [...content];
+    let dotIndex = textList.indexOf('.');
+
+    let textSlice = dotIndex > 0 ? textList.slice(0, dotIndex) : textList;
+    let dotSlice = dotIndex > 0 ? textList.slice(dotIndex) : [];
+
+    textSlice = textSlice.filter((item) => item != ',');
+    
+    let arr = textSlice.reverse().reduce((acc, item, i) => {
+        i > 0 && i % 3 == 0 ? acc.push(',', item) : acc.push(item);
+        return acc;
+    }, []);
+
+    content = arr.reverse().join('') + dotSlice.join('');
+    return;
+}

5c059c4540ec8074cfaa5aaff9a2da3610ddc15c
Juan Stevan <juanstevan@live.com>
2025-08-25 01:16:22 -0400

    Feat: Keys call now works for the numebers and clean display

--- script.js
+++ script.js
@@ -2,14 +2,27 @@ const numbers = document.querySelectorAll('.num');
 const clear = document.getElementById('clear');
 const display = document.getElementById('display');
 
-numbers.forEach((number) => {
-    number.addEventListener('click', (event) => {
-        display.textContent += event.target.textContent;
-    });
+Object.defineProperty(window, 'content', {
+    get: () => display.textContent,
+    set: (n) => display.textContent = n
+});
+
+numbers.forEach((number) => 
+    number.addEventListener('click', function() {
+        if (this.id == 'dot' && content.includes('.')) return;
+        content += this.textContent
+    })
+);
+
+document.addEventListener('keydown', (event) => {
+    let key = event.key;
+    if (key == '.' && content.includes('.') || key !== '.' && isNaN(+key)) return;
+
+    key == ' ' ?
+        content = '' : 
+        content += key
 });
 
 clear.addEventListener('click', (event) => {
-    display.textContent = "";
+    content = "";
 });
-
-
